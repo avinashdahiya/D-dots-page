b33d26e83088d2733c460969493b9a470c7a65d8
avinashdahiya <addahiya2002@gmail.com>
2025-04-15 15:02:56 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+let backToTopButton = document.getElementById("backToTop");
+window.onscroll = function () {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+backToTopButton.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+};
